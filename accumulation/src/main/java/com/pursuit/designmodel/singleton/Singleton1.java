cea91e033824069604bc4f746c44851c491052b8
wangjn1130 <wangjn1130@163.com>
2016-08-17 15:32:41 +0800

    序列化验证

--- accumulation/src/main/java/com/pursuit/designmodel/singleton/Singleton1.java
+++ accumulation/src/main/java/com/pursuit/designmodel/singleton/Singleton1.java
@@ -1,6 +1,7 @@
 package com.pursuit.designmodel.singleton;
 
-public class Singleton1 {
+import java.io.Serializable;
+public class Singleton1 implements Serializable{
 	private Singleton1 instance = null;
 	private Singleton1(){
 		System.out.println("singleton init  NO.1");
@@ -11,7 +12,18 @@ public class Singleton1 {
 	public static Singleton1 getInstance(){
 		return Singleton1Holder.instance;
 	}
-	public static void test(){
+	public void test(){
 		System.out.println("this is a test");
 	}
+	//解决序列化问题 [实现了序列化接口]
+	private Object readResolve() {     
+        return Singleton1Holder.instance;     
+    }
+	//解决反射问题
+//	private static Class getClass(String classname)   throws ClassNotFoundException {
+//		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();     
+//		if(classLoader == null) 
+//			classLoader = Singleton1.class.getClassLoader(); 
+//		return (classLoader.loadClass(classname)); 
+//   }
 }
--- accumulation/src/main/java/com/pursuit/designmodel/singleton/SingletonMain.java
+++ accumulation/src/main/java/com/pursuit/designmodel/singleton/SingletonMain.java
@@ -1,12 +1,23 @@
 package com.pursuit.designmodel.singleton;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 public class SingletonMain {
-	public static void main(String[] args) {
-		test3();
+	public static void main(String[] args) throws Exception {
+		//test3();
+		//测试序列化
+		//test4();
+		//测试反射
+		test5();
 	}
 	//1.静态内部类
 	public static void test1(){
-		Singleton1.test();
+		//Singleton1.test();
 		Singleton1 instance = Singleton1.getInstance();
 	}
 	//2.枚举类型
@@ -19,8 +30,9 @@ public class SingletonMain {
 	}
 	//3.双重校验锁
 	public static void test3(){
+		
+		//多线程环境
 		new Thread(new Runnable() {
-			
 			@Override
 			public void run() {
 				Singleton3 instance = Singleton3.getInstance();
@@ -28,5 +40,29 @@ public class SingletonMain {
 			}
 		}).start();;
 		Singleton3 instance1 = Singleton3.getInstance();
+	}
+	//4.序列化环境验证
+	public static void test4() throws Exception{
+		Singleton1 instance = Singleton1.getInstance();
+		System.out.println(instance);
+		ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("d:/instance.txt"));
+		output.writeObject(instance);
+		output.flush();
+		output.close();
+		
+		ObjectInputStream input = new ObjectInputStream(new FileInputStream("d:/instance.txt"));
+		Singleton1 readObject = (Singleton1) input.readObject();
+		System.out.println(readObject);
+		System.out.println(readObject==instance);
+		System.out.println(readObject==null);
+		readObject.test();
+		instance.test();
+	}
+	//5.测试反射
+	public static void test5() throws Exception{
+		Class<?> clazz = Class.forName("com.pursuit.designmodel.singleton.Singleton1");
+		Constructor<?> constructor = clazz.getDeclaredConstructor();
+		constructor.setAccessible(true);
+		Singleton1 instance = (Singleton1) Class.forName("com.pursuit.designmodel.singleton.Singleton1").newInstance();
 	}
 }
