eeac974a5cbb16f5d04f1179c529dce107b64a1e
wangjn1130 <wangjn1130@163.com>
2016-08-18 14:10:52 +0800

    观察者意义

--- accumulation/src/main/java/com/pursuit/designmodel/observer/jdk/JDKMain.java
+++ accumulation/src/main/java/com/pursuit/designmodel/observer/jdk/JDKMain.java
@@ -1,5 +1,9 @@
 package com.pursuit.designmodel.observer.jdk;
 
+/**
+ * 观察者模式可以让多线程监控问题，以单线程的方式完成处理
+ *
+ */
 public class JDKMain {
 	public static void main(String[] args) {
 		MySubject observale = new MySubject();
